5ff53427797ab79306d510f2ff225d6dc1cb6229
Charlie <itscharlieliu@gmail.com>
2021-07-05 21:07:59 -0700

    WIP create static webpage that streams video

--- src/webserver.py
+++ src/webserver.py
@@ -3,5 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def test():
+    # https://blog.miguelgrinberg.com/post/video-streaming-with-flask
+    # Use this to create a static webpage that can stream a video
     return "<p>Hello, World!</p>"
