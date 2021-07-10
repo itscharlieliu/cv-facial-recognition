d6614a5de12a74f93d0db5aab4ecf2d97ad28710
Charlie <itscharlieliu@gmail.com>
2021-07-09 17:53:58 -0700

    Try to create webpage using example html

--- src/webserver.py
+++ src/webserver.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, Response
-from camera import Camera
 
 app = Flask(__name__)
 
@@ -7,15 +6,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-def gen(camera):
+def generate_frame():
     while True:
-        frame = camera.get_frame()
+        # Testing
+        with open("test.jpeg", "rb") as image:
+            f = image.read()
+            frame = bytearray(f)
+
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(Camera()),
+    return Response(generate_frame(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
