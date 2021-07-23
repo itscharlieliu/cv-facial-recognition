c56650e7fd931c4a6f76d11e44e350dbe1c8ea58
Charlie <itscharlieliu@gmail.com>
2021-07-22 21:48:32 -0700

    debugging...

--- src/main.py
+++ src/main.py
@@ -7,19 +7,29 @@ import os
 
 print(f"Running main app: {__name__}")
 
+
+def test():
+    print("this should only run once")
+    app.run(host="0.0.0.0", debug=True)
+
+
 if __name__ == "__main__":
     # Change working directory
     abspath = os.path.abspath(__file__)
     dname = os.path.dirname(abspath)
     os.chdir(dname)
 
-    webserver = multiprocessing.Process(
-        target=app.run, kwargs={"host": "0.0.0.0", "debug": True}
-    )
+    # webserver = multiprocessing.Process(
+    #     target=app.run, kwargs={"host": "0.0.0.0", "debug": True}
+    # )
+    webserver = multiprocessing.Process(target=test)
     cv_app = multiprocessing.Process(target=start)
 
     print("Starting webserver...")
-    webserver.start()
+    value = webserver.start()
+
+    print(f"Got: {value}")
+
     # time.sleep(1)
 
     # print("Starting cv app")
