1bb918d06b535202f690974662ab7a45322bf0d6
Charlie <itscharlieliu@gmail.com>
2021-07-27 21:38:08 -0700

    Debugging ws message not being sent

--- src/cv_app.py
+++ src/cv_app.py
@@ -7,25 +7,18 @@ import os
 
 
 def start():
-    print(os.getpid())
-
     # Communicate with the webserver
     ws = create_connection(f"ws://127.0.0.1:5000/video_in")
 
     img = cv.imread("images/airplane.jpg")
 
     # print(numpy_to_img(img))
-
-    # ws.send(b"1")
-    ws.send(b"hello")
-
-    print("got here 1")
-
     time.sleep(1)
 
-    ws.send(b"world")
+    ws.send(b"2")
 
-    print("got here 2")
-    # ws.send(b"2")
+    ws.send(numpy_to_img(img))
+
+    print("Got here")
 
     ws.close()
